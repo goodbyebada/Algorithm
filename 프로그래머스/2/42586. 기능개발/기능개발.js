66667ce0512af3a4acc76cf0fead29c3537fd81c
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-02-21 17:01:53 +0900

    [level 2] Title: 기능개발, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,46 +1,27 @@
 
 
-function compare(prev, front){
-    return prev >= front
-}
-
+// 🛑 max 를 찾는 것이 핵심인 문제
 function solution(progresses, speeds) {
-    const answer = [];
-    const restDay = [];    
+    const answer = [0];
+    const restDay = progresses.map((progress, idx) => Math.ceil((100-progress)/speeds[idx]))
     
+    let max = restDay[0];
     
-//     restDay 배열
-    progresses.forEach((progress, idx) =>{
-        const rest = Math.ceil((100-progress)/speeds[idx])
-        restDay.push(rest)
-    } )
-    
-    
-    let max = -100;
-    
-    for(let i = 0; i < restDay.length; i++){
-        if( max <  restDay[i]){
-            max = restDay[i]
-        } else{
-            restDay[i] = max
-        }
-    }
-    
-    console.log(restDay)
-    
-    
-    let count = 1;
-    for(let i = 0; i <= restDay.length-1; i++){
+    for(let i = 0, j =0;i < restDay.length;i++){
         
-        if(restDay[i] !== restDay[i+1]){
-            answer.push(count)
-            count = 1;
-            continue;
+        if(restDay[i] > max){
+            max = restDay[i]
+            
+//             index 증가
+            answer[++j] = 1
+            
+        }else{
+            answer[j] +=1
         }
-        count++;
+        
     }
-
-
+    
+    
   
     return answer;
 }
