31db46c0b90233f34227232e1ead700ab0c6f8fd
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-03-19 21:25:18 +0900

    [Silver III] Title: 01타일, Time: 244 ms, Memory: 29424 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.js
@@ -0,0 +1,21 @@
+/**
+ * 1타일 || 00 타일
+ *
+ * 📌 순서를 고려해야하나 고민했는데,  + 1 || 00 타일로 끝나면 겹치지 않는다.
+ 
+ *
+ */
+
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = +require("fs").readFileSync(path).toString().trim();
+const dp = Array.from({ length: input }, () => 0);
+
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= input; i++) {
+  dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+}
+
+console.log(dp[input]);
