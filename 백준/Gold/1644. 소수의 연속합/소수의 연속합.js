0eb61fd1f92760fd4f5be9c1a304b9c2ba3c6d37
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-04-02 17:14:01 +0900

    [Gold III] Title: 소수의 연속합, Time: 400 ms, Memory: 41112 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.js
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.js
@@ -1,35 +1,79 @@
-const N = Number(require('fs').readFileSync('/dev/stdin').toString().trim());
+//  소수의 연속 합
 
-const isPrime = Array(N + 1).fill(true);
-isPrime[0] = isPrime[1] = false;
+/**
+ * 1. 연속된 소수의 합으로 자연수를 나타내야한다.
+ *
+ * 자연수가 주어졌을대, 연속된 소수의 합 경우의 수를 구해야한다.
+ *
+ *
+ */
 
-for (let i = 2; i <= Math.sqrt(N); i++) {
-  for (let j = i * i; j <= N; j += i) {
-    isPrime[j] = false;
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = +require("fs").readFileSync(path).toString().trim();
+
+// 에라토스테네스의 체 알고리즘
+function getIsDemicalList(num) {
+  const isDemical = new Array(num + 1).fill(true);
+  //   0과 1은 사용 X
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    // i*i로 시작하면 중복된 숫자를 제외할 수 있다.
+    for (let j = i * i; j <= num; j += i) {
+      isDemical[j] = false;
+    }
   }
+
+  return isDemical;
 }
 
-const primes = isPrime.reduce((acc, v, i) => {
-  if (v) {
-    acc.push(i);
+function nextVal(isDemical, curIdx) {
+  for (let idx = curIdx + 1; idx < isDemical.length; idx++) {
+    if (isDemical[idx]) {
+      return idx;
+    }
   }
-  return acc;
-}, []);
 
-let sum = 0;
-let count = 0;
-let i = 0;
-let j = 0;
-while (j < primes.length) {
-  sum += primes[j];
-  while (sum > N) {
-    sum -= primes[i];
-    i++;
-  }
-  if (sum === N) {
-    count++;
-  }
-  j++;
+  return isDemical.length;
 }
 
-console.log(count);
+function twoPointerProcess(isDemical, num, count) {
+  let start = 2;
+  let end = 2;
+  let sum = 0;
+
+  //  sum > num start가 움직인다.
+  //  sum < num 일 때 end가 움직인다.
+  //   end len-1이고 sum < num이라면 break
+  //
+  //   start 가 len -1이라면 break -> 같은 방향으로
+
+  while (1) {
+    if (sum === num) count++;
+
+    if (sum < num) {
+      if (end === isDemical.length) break;
+
+      sum += end;
+
+      end = nextVal(isDemical, end);
+      //   더하고 나서도 작다면 end == len이 되기  때문에 break
+    } else {
+      if (start === isDemical.length) break;
+      sum -= start;
+      start = nextVal(isDemical, start);
+    }
+  }
+
+  return count;
+}
+
+function sol() {
+  let count = 0;
+  let isDemical = [];
+
+  isDemical = getIsDemicalList(input);
+
+  count = twoPointerProcess(isDemical, input, count);
+  console.log(count);
+}
+
+sol();
