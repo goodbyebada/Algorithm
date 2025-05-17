063256cd835113c0d1d78523755059ddfe6a4e01
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-05-18 00:56:00 +0900

    [level 2] Title: 요격 시스템, Time: 498.80 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,42 +1,28 @@
 function solution(targets) {
     var answer = 0;
     
-//     targets 정렬
-//     end 끝날 때 요격 미사일을 사용할 수 있음
-    
-//     e 기준으로 오름차순 
+    //     e 기준으로 오름차순 
     targets = targets.sort((a,b) => {
         if(a[1] === b[1]) return a[0] - b[0]
         return a[1] - b[1]
     })
     
-    
+//     end 갱신될 때 answer 해준다.
     
     let index = 0
-    let count = 0
-    let e =  targets[index][1]
+    let currEnd =  targets[index][1]
     
+    index++
+    answer++
     
-//     마지막 인덱스까지 돈다.
-//     처음 끝나는 end  지점에서 count++한다.
     while(index < targets.length){
+        const [s, e] = targets[index++]
         
-        if(index === 0) {
-            count++
-            index++
-            continue;
+        if(s >= currEnd){
+            currEnd = e
+            answer++
         }
-        
-        
-        const [nextStart, nextEnd] = targets[index++]
-        if(nextStart < e) continue;
-
-//         처음 갱신될때 세어준다.
-            count++
-            e = nextEnd;
-        
     }
     
-    
-    return count;
+    return answer;
 }
