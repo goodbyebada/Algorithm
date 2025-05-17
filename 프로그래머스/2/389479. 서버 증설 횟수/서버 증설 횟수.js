03d07089733e79c05951773eb76afc6203114278
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-05-17 19:25:53 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.24 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
@@ -1,36 +1,37 @@
-// 서버 최소 몇 번 증설
+
+
+
+//  이 시간대 사람들의 (players)/m 개가 필요함
+//  현재 있는 서버 먼저 업데이트  -> 사라지는 서버 
+// 추가 증설 서버 = 필요한 서버 - (현재 있는 서버)
+//  필요한 서버 > 현재 있는 서버 => 증설하는거임
 
 function solution(players, m, k) {
     var answer = 0;
+    let index = 0
     
-    const allCount = new Array(players.length).fill(0)
-    const buildCount = new Array(players.length).fill(0)
     
-    players.forEach((elem, idx) => {    
-    const shouldBuildCount = Math.floor(elem / m)
+    let currServer = 0
+    let addedServer = 0
+    const timeOut = Array(players.length).fill(0);
     
-//    실제 빌드된 서버 수
-    const buildServerCount =  shouldBuildCount - allCount[idx]
-
-    if( shouldBuildCount > 0 && buildServerCount > 0){
-        buildCount[idx] = buildServerCount;
+    
+    for(let time =0; time < players.length; time++){
+        const remove = timeOut[time]
         
-        for(let i = idx ;  i < idx+ k; i++){
-            // NaN case
-            if(i >= players.length) break;
-            allCount[i] += buildServerCount
+        if(remove){
+            currServer-= remove
         }
+        
+        const need = Math.floor(players[time]/m) - currServer
+        
+        if(need <= 0) continue;
+    
+        
+        timeOut[time+k] = need
+        addedServer+= need
+        currServer+= need
     }
-
-        
-        
-    })
-    
-    
-    const result = buildCount.reduce((acc, curr, idx) =>
-        acc+= curr
-    )
-
-    
-    return result;
+         
+    return addedServer;
 }
