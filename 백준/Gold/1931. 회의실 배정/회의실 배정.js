255cfb40b53fa4d70743e8ec04cea378601eda7d
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-05-18 00:47:46 +0900

    [Gold V] Title: 회의실 배정, Time: 336 ms, Memory: 43224 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.js
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.js
@@ -1,24 +1,6 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(path).toString().trim().split("\n");
 
-/**
- *
- * 1. 최대 개수이기 때문에 중간에 빵꾸가 나도 된다. -> 시간 폭이 작은 걸 우선순위로 두고 해야한다.
- * 2. 회의 시간 순으로 정렬한다.
- * 3. 순서대로 조회한다. -> 범위가 겹치면 버린다. -> 이전 데이터를 알고 있어야한다.
- *
- * 4. 갱신되는 끝나는 시간이 < 2^31 -1 조건을 만족할 때까지 반복
- *
- * O(NlogN)
- */
-
-// 🚨 많은 회의실을 넣으려면 많은 회의시간을 확보해야한다 -> 빨리 끝나는 회의를 잡아야한다.
-
-/**
- * 끝나는 시간을 기준으로 정렬한다.
- * 다음 회의를 고를 때 이전의 끝나는 시간보다 크고, 제일 빨리 끝나는 회의를 고른다.
- */
-
 const data = input.slice(1).map((elem) => elem.split(" ").map(Number));
 
 // 끝나는 회의시간 기준으로 정렬
@@ -26,35 +8,29 @@ const data = input.slice(1).map((elem) => elem.split(" ").map(Number));
 // 배열 정렬 (끝나는 시간 기점)
 const sorted = data.sort((a, b) => (a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]));
 
-const END_TIME = Math.pow(2, 31) - 1;
-let index = 0;
-let count = 0;
-let endTime = sorted[index][1];
-count++;
-index++;
+function solution(list) {
+  let count = 0;
+  let index = 0;
+  let currEnd = list[index][1];
+  index++;
+  count++;
 
-// while (endTime < END_TIME) {
-//   if (index >= sorted.length) break;
+  //    빨리 끝나는 시간을 기준으로 오름차순
+  //     가장 빨리 끝나는 회의를 연속해서 찾아야한다.
+  //     - 겹치면 안됨, -> nextstart < currEnd
+  //     아니라면 선택한다. currEnd 업데이트
+  //     list를 순회한다.
 
-//   let [currStart, currEnd] = sorted[index];
+  while (index < list.length) {
+    const [nStart, nEnd] = list[index];
+    index++;
 
-//   // 회의시간이 겹치지 않는지 확인한다.
-//   if (endTime <= currStart) {
-//     // 끝나는 시간 기준으로 정렬했기 때문에, 위 조건을 만족한다면 남은 회의 중 가장 빠르게 끝나는 회의!
-//     endTime = currEnd;
-//     count++;
-//   }
-
-//   // 갱신
-//   index++;
-// }
-
-for (let i = 1; i < sorted.length; i++) {
-  let [currStart, currEnd] = sorted[i];
-  if (endTime <= currStart) {
-    endTime = currEnd;
+    if (nStart < currEnd) continue;
+    currEnd = nEnd;
     count++;
   }
+
+  return count;
 }
 
-console.log(count);
+console.log(solution(sorted));
