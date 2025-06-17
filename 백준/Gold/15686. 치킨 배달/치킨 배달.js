5cebf7ff798d2bd427e78b28e13bda5ccf863ad4
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-06-17 14:30:36 +0900

    [Gold V] Title: 치킨 배달, Time: 180 ms, Memory: 12428 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.js
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.js
@@ -1,26 +1,4 @@
-// 도시의 치킨 거리가 가장 작게 될 지 구해라
-// M개를 고르고, 치킨 거리가 가장 적게 된다.
-
-// 치킨 거리:집과 가장 가까운 치킨집의 사이
-
-// 각 집마다의 치킨 거리를 계산하고 => 완탐
-// M개의 합 최솟값이어야한다.
-
-// => 이렇게 생각한 이유는?
-//  어떤 치킨 집이 사라지냐에 따라 각 집의 치킨 거리가 변경된다.
-// 때문에 모든 치킨 집과의 거리가 있어야한다.
-
-// 1. 치킨집의 위치, 각 집의 위치를 찾는다.
-// 집 (인덱스 - 위치)
-// 치킨 집 (인덱스 - 위치)
-
-// 2. 치킨 집 * 각 집 거리를 계산한다.
-// 3. 0번 부터 ~ 치킨집까지 M개를 선택해 모든 경우를 구해본다.
-// 행 (치킨 인덱스) 열(집)
-
-// (전체 치킨)C(M개 치킨) * (N*M => 각 집마다 최소 치킨 거리 구하기)
-
-// 1H 12M 틀렸습니다.
+// 1H 22M
 
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs")
@@ -63,51 +41,33 @@ function calcDist(shops, homes) {
 }
 
 function findMinDist(dist, M) {
-  // row M개씩 계산하며 최대값
-
   let MIN = Infinity;
   const count = dist.length;
   const homeCount = dist[0].length;
-  const used = Array(count).fill(false);
+  // const used = Array(count).fill(false);
+  const stack = [];
 
-  //   👹 왜 착각했지? 순서대로 M개일리가 없잖아
-  //   백트래킹
-
-  //dfs 해도 스택 오버플로우 안 될 것 같지만 스택으로 풀자
-  //   조합을 구해야한다.
-  function backTracking(depth, used, shops) {
+  function backTracking(start, depth) {
     // 수행한다.
     if (depth === M) {
       let nowMin = 0;
       for (let home = 0; home < homeCount; home++) {
-        nowMin += calcMyChickDist(dist, home, shops);
+        nowMin += calcMyChickDist(dist, home, stack);
         if (nowMin > MIN) break;
       }
-      //   console.log(shops);
-      //   console.log(nowMin);
 
       if (nowMin < MIN) MIN = nowMin;
       return;
     }
 
-    for (let i = 0; i < count; i++) {
-      if (used[i]) continue;
-      used[i] = true;
-      //   shops.push(i);
-      backTracking(depth + 1, [...used], [...shops, i]);
+    for (let i = start; i < count; i++) {
+      stack.push(i);
+      backTracking(i + 1, depth + 1);
+      stack.pop();
     }
   }
 
-  backTracking(0, used, []);
-
-  function combination() {
-    // 1. stack에 1일때 , used 사용된 거 넣는다.
-    // 2. 그 같은 used를 참고해 다음 index를 넣는다.
-    // 3. 위를 반복한다.
-    // 4, M과 스택의 길이가 같아질 때 최솟값 수행 로직을 수행한다.
-    // 그 다음 pop한 뒤 used가 되지 않은 숫자를 넣는다.
-    // used가 다 사용되
-  }
+  backTracking(0, 0);
 
   return MIN;
 }
@@ -128,15 +88,24 @@ function sol(input) {
   let index = 0;
   const [N, M] = input[index++].split(" ").map(Number);
   let map = [];
-
-  for (let row = 0; row < N; row++) {
-    map.push(input[index++].split(" ").map(Number));
-  }
-
   const shops = [];
   const homes = [];
 
-  findPos(map, N, shops, homes);
+  // 한번에 ✨ pos 찾기
+  for (let row = 0; row < N; row++) {
+    map.push(input[index++].split(" ").map(Number));
+    map[row].forEach((value, col) => {
+      if (value === 1) {
+        homes.push([row, col]);
+      }
+
+      if (value === 2) {
+        shops.push([row, col]);
+      }
+    });
+  }
+
+  // findPos(map, N, shops, homes);
   const dist = calcDist(shops, homes);
   const MIN = findMinDist(dist, M);
   console.log(MIN);
