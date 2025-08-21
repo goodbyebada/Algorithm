62e5d745657e74ba5615e8288c9dd92b0d0cd4c2
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-08-21 23:34:20 +0900

    [level 2] Title: 숫자 변환하기, Time: 244.55 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,4 +1,31 @@
 function solution(x, y, n) {
-    var answer = 0;
-    return answer;
+    var answer = -1;
+    
+//     기록
+    const dp = {}
+    
+    const list = [x]
+    let index = 0
+    dp[x] = 0 
+    
+    while(list.length > index ){
+        let curr = list[index++]
+        
+        if(curr === y) answer = dp[curr]
+        
+        for(let nVal of [curr + n, curr *2, curr*3]){
+            
+            
+//             조건확인, 중복 확인
+            if(nVal > y || dp[nVal]) continue
+            list.push(nVal)
+            dp[nVal] = dp[curr] +1    
+        }
+        
+    }
+    
+    
+    return answer
+    
+
 }
