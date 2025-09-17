f0620425722aea46495fe8f15f911444f842d603
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-09-17 23:44:30 +0900

    [level 2] Title: 전화번호 목록, Time: 131.70 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,30 @@
+// 한 번호가 다른 번호의 접두어인 경우
+// phone_book 10^7
+
+// 접두사가 되면 -> false
+// 짧은 것부터 검사..?
+// 접두어.
+
+// 헉..어렵다..!
+//  N + N-1+ N-2 => N^2
+// 시간 초과
+// 선형으로...
+// 자릿수로 잘라봐..? 정렬 시킴 
+// 짧은것부터 자릿수 순으로 시작
+//  1 + 2 + ~ N => N^2
+// 선형으로 해 
+// 앞에 있는거랑 비교 해. 
+
+function solution(phone_book) {
+    // 정렬하면 접두어 관계에 있는 번호들이 인접하게 배치됨
+    phone_book.sort();
+    
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        // 다음 번호가 현재 번호로 시작하는지만 확인하면 됨
+        if (phone_book[i + 1].startsWith(phone_book[i])) {
+            return false;
+        }
+    }
+    
+    return true;
+}
