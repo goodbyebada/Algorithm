9055fe09d594c8d6d2e5b0119d4777eede5422c6
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-10-03 23:58:43 +0900

    [level 2] Title: 가장 큰 수, Time: 167.07 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,78 +1,20 @@
-
-
-
-
-// a와 b를 str로 만들어비교한다.
-// 길이가 긴 str의 idx로 접근한다.
-// 만약 짧은 str undefined가 나왔을시, 긴 str[idx-1] < str[idx] 일때 긴 str이 max 된다.
-// 아니라면 a 가 max
-// a와 b중 뭐가 긴지 알아야한다.
-
-//  max = a ? 혹은 b인지 알아야함
-//  max 가 a 라면 -1
-//  b 라면 1 출력
-// 같다면 1 출력
-
-// 자리를 바꿔야한다면 return 1
-// 아니라면 -1
-
-
-// 내림차순
-// ? 왜안되지?
-function failCompare(a,b){
-    
-    const aString = `${a}`
-    const bString = `${b}`
-    const maxlenStr = aString.length < bString.length ? bString : aString;
-    const minLenStr = maxlenStr === aString ? bString: aString;
-    let result = ''
- 
-    for(let idx = 0; idx < maxlenStr.length; idx++){
-        
-        const min = Number(minLenStr[idx]);
-        const max = Number(maxlenStr[idx]);
-        
-        if(min === max) continue;
-        if(Number.isNaN(min)){
-            
-            if(Number(maxlenStr[idx-1]) < max) result = maxlenStr
-            else result = minLenStr;
-            break
-        }
-        
-        
-        if(min < max ) result =  maxlenStr;
-        else result = minLenStr;
-        
-        break;
-    }
-    
-    
-//     result가 Max인 값
-    (a, b)
-    
-    if(result === aString) return 1;
-    else return -1
-    
-
-}
-
-
-function compare(a, b) {
-    const strA = a.toString();
-    const strB = b.toString();
-    return (strB + strA) - (strA + strB);
-}
-
-// 정수를 이어붙여 만들 수 있는 가장 큰 수
-// 모든 배열의 원소를 다 사용해야한다.
 function solution(numbers) {
-    var answer = '';
-    
-    numbers.sort((a,b) => compare(a,b)).forEach((elem) => answer+= elem)
-    console.log(numbers)
 
+    const strNumbers = numbers.map(n => n.toString());
     
-//     0인경우 0으로 출력해야한다.
+    // 커스텀 정렬: a+b vs b+a 비교
+    strNumbers.sort((a, b) => {
+        const order1 = a + b;  
+        // "3" + "30" = "330"
+        
+        const order2 = b + a;  
+        
+        // "30" + "3" = "303"
+        return order2 - order1;  // 내림차순 (큰 것이 앞으로)
+    });
+    
+    const answer = strNumbers.join('');
+    
+    // 모든 숫자가 0인 경우
     return answer[0] === '0' ? '0' : answer;
 }
