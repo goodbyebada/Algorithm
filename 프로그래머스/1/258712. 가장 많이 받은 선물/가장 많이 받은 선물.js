cdf0561f4eeb86de34eb89ab65156eb4e2e56439
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-10-04 17:33:16 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 30.24 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -10,130 +10,93 @@
 // "A B"
 // 선물 준 사람 // 선물 받은 사람
 
-
 // 배열 사용 => 이름 - index 번호 map
 // 1. 준 사람/ 받은 사람 => 데이터를 정리한다. 이차원 배열
 // 2. 사람 별 준 선물 / 받은 선물 => 계산 해 선물지수 갱신
 // 3. 한 사람마다 비교  -> 중복 이슈 -> muzy + ryan === ryan + muzy => 문자열 set에 넣어버려서 중복 체크
 // 4. 1차원 배열에 받을 선물의 개수 입력
 
-
 function solution(friends, gifts) {
-    var answer = 0;
-    const nameSet = new Set()
-    
-    const N = friends.length
-    
-//     [muzi][ryan]
-    
-//     선물을 준 사람 선물을 받은 사람
-    const giftMap  = {}
-    
-//     선물을 받은 사람 선물을 준 사람
-    const getMap  = {}
-    
-    for(let names of gifts){
-        
-//         준 사람, 받은 사람
-        const [buyP, getP ] = names.split(" ")
-        
-        if(!giftMap[buyP]){
-            giftMap[buyP] = { total : 0}
+  var answer = 0;
+  const nameSet = new Set();
+
+  const N = friends.length;
+
+  //     [muzi][ryan]
+
+  //     선물을 준 사람 선물을 받은 사람
+  const giftMap = {};
+
+  //     선물을 받은 사람 선물을 준 사람
+  const getMap = {};
+
+  for (let names of gifts) {
+    //         준 사람, 받은 사람
+    const [buyP, getP] = names.split(" ");
+
+    giftMap[buyP] = giftMap[buyP] || { total: 0 };
+    getMap[getP] = getMap[getP] || { total: 0 };
+
+    giftMap[buyP][getP] = (giftMap[buyP][getP] || 0) + 1;
+    getMap[getP][buyP] = (getMap[getP][buyP] || 0) + 1;
+
+    giftMap[buyP].total += 1;
+    getMap[getP].total += 1;
+  }
+
+  console.log(giftMap, getMap);
+
+  //     받은 선물 , 준 선물 갱신 => 선물 지수 갱신 위해
+
+  const giftJisu = {};
+
+  for (const name of friends) {
+    //         준 선물 - 받은 선물
+
+    //         undefiend라면
+    const giveGiftCnt = !giftMap[name] ? 0 : giftMap[name]["total"];
+
+    //         받은 선물 수 getMap
+    const getGiftCnt = !getMap[name] ? 0 : getMap[name]["total"];
+
+    giftJisu[name] = giveGiftCnt - getGiftCnt;
+  }
+
+  const set = new Set();
+
+  const nextMonthGift = {};
+
+  for (let name of friends) {
+    nextMonthGift[name] = 0;
+  }
+
+  for (let i = 0; i < N; i++) {
+    for (let j = i + 1; j < N; j++) {
+      let main = friends[i];
+      let freind = friends[j];
+
+      const giveGiftCnt = (giftMap[main] && giftMap[main][freind]) || 0;
+      const getGiftCnt = (giftMap[freind] && giftMap[freind][main]) || 0;
+
+      //           지수 비교
+      if (giveGiftCnt - getGiftCnt === 0) {
+        if (giftJisu[main] === giftJisu[freind]) continue;
+
+        if (giftJisu[main] > giftJisu[freind]) {
+          nextMonthGift[main] += 1;
+          continue;
         }
-        
-        if(!getMap[getP]){
-            getMap[getP] = { total : 0}
-        }
-        
-        
-        
-//         선물 받은 사람
-        if(!giftMap[buyP][getP]) giftMap[buyP][getP] = 1
-        else giftMap[buyP][getP] += 1
-    
-        
-        
-        
-//         선물 준 사람 map
-        if(!getMap[getP][buyP]) getMap[getP][buyP] = 1
-        else getMap[getP][buyP] += 1
-        
-        giftMap[buyP].total += 1
-        getMap[getP].total += 1
+
+        nextMonthGift[freind] += 1;
+        continue;
+      }
+
+      if (giveGiftCnt > getGiftCnt) nextMonthGift[main] += 1;
+      else nextMonthGift[freind] += 1;
     }
-    
-    
-    console.log(giftMap,getMap )
-    
-    
-//     받은 선물 , 준 선물 갱신 => 선물 지수 갱신 위해
-    
-    
-    const giftJisu = {}
-    
-    
-    for(const name of friends){
-//         준 선물 - 받은 선물
-        
-//         undefiend라면 
-        const giveGiftCnt = !giftMap[name] ? 0 : giftMap[name]["total"]
-        
-//         받은 선물 수 getMap
-        const getGiftCnt = !getMap[name] ? 0 : getMap[name]["total"]
-        
-        giftJisu[name] = giveGiftCnt - getGiftCnt
-    }
-    
-    const set = new Set()
-    
-    const nextMonthGift = {}
-    
-    for(let name of friends){
-        nextMonthGift[name] = 0
-    }
-    
-    
-    
-    for(let i = 0; i < N; i++){
-        for(let j = i+1; j < N; j++){
-            
-            let main = friends[i]
-            let freind = friends[j]
-            
-            const giveGiftCnt = (giftMap[main] && giftMap[main][freind]) || 0
-            const getGiftCnt = (giftMap[freind] && giftMap[freind][main]) || 0
-            
-          
-//           지수 비교 
-          if(giveGiftCnt - getGiftCnt === 0){
-              if(giftJisu[main] === giftJisu[freind] ) continue;
-              
-              if( giftJisu[main] > giftJisu[freind]){
-                  nextMonthGift[main] += 1
-                  continue;
-              }
-              
-              nextMonthGift[freind]+= 1
-              continue;    
-          }
-            
-        if(giveGiftCnt > getGiftCnt) nextMonthGift[main] +=1 
-        else nextMonthGift[freind] +=1 
-            
-            
-            
-        }
-        
-        
-    }
-    
-    const result= Object.entries(nextMonthGift).sort((a, b) => b[1] - a[1])
-  
-    
-    
-    
-    
-    
-    
-    return result[0][1];
+  }
+
+  const result = Object.entries(nextMonthGift).sort((a, b) => b[1] - a[1]);
+
+  return result[0][1];
 }
